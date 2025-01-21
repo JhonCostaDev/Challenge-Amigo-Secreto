e56012556e774576006d778c2b3bdf03cf44c09d
Jonathan Costa <jhoncosta2@gmail.com>
2025-01-21 11:05:28 -0300

    add array amigos e function addAmigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+//array para armazenar os amigos
+
+let amigos = [];
+
+//função para adicionar os amigos na lista.
+function adicionarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    amigos.push(amigo);
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = amigos;
+    document.getElementById('amigo').value = '';
+}
+
